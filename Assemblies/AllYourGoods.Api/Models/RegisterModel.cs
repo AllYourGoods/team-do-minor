e36cfcb08fafc7da401b970524c04259a53e9ef8
M.Tarkhany <75487802+MT0993790@users.noreply.github.com>
2024-10-03 21:16:06 +0200

    Created RegisterModel and LoginModel

---
+++ Assemblies/AllYourGoods.Api/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AllYourGoods.Api.Models
+{
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
