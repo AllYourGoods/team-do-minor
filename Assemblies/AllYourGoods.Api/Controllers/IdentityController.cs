967bb3cfc531348c130f7c41a25dc3f467541b06
edfreded <moesabajdid@hotmail.com>
2024-10-05 22:15:20 +0200

    Story-22 Docker container boot order fixed

--- Assemblies/AllYourGoods.Api/Controllers/IdentityController.cs
+++ Assemblies/AllYourGoods.Api/Controllers/IdentityController.cs
@@ -94,15 +94,15 @@ public class AuthController : ControllerBase
     }
 }
 
-public class LoginModel
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
+// public class LoginModel
+// {
+//     public string Username { get; set; }
+//     public string Password { get; set; }
+// }
 
-public class RegisterModel
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
-}
+// public class RegisterModel
+// {
+//     public string Username { get; set; }
+//     public string Password { get; set; }
+//     public string Email { get; set; }
+// }
---
+++ Assemblies/AllYourGoods.Api/Models/Register.cs
@@ -0,0 +1,8 @@
+﻿namespace AllYourGoods.Api.Models;
+
+public class RegisterModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
