29adc429d7ec06bc8c475971e4b41d52bc7ab869
Frits <0765612@hr.nl>
2024-10-05 15:28:29 +0200

    Test deployment

---
+++ Assemblies/AllYourGoods.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using AllYourGoods.Api.Middleware;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace AllYourGoods.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    private static List<Type>? _excludedControllers;
+
+    public static IApplicationBuilder EnableBuffering(this IApplicationBuilder builder) => builder.UseMiddleware<EnableBufferingMiddleware>();
+
+    /// <summary>
+    /// NOTE: You need to call this middleware method AFTER IApplicationBuilder.UseRouting AND BEFORE IApplicationBuilder.UseEndpoints,
+    /// Also, request buffering needs to be enabled for this functionality to work! Use IApplicationBuilder.EnableBuffering()
+    /// </summary>
+    /// <param name="builder">Your application's IApplicationBuilder</param>
+    /// <param name="exclude">List of controller types to exclude from body logging</param>
+    /// <returns>IApplicationBuilder</returns>
+    public static IApplicationBuilder EnableRequestBodyLoggingMiddleware(this IApplicationBuilder builder, List<Type>? exclude = null)
+    {
+        _excludedControllers = exclude;
+        builder.UseWhen(ExcludeControllersDelegate, b => b.UseMiddleware<EnableRequestBodyLoggingMiddleware>());
+
+        return builder;
+    }
+
+    private static bool ExcludeControllersDelegate(HttpContext context)
+    {
+        if (_excludedControllers is null)
+        {
+            return true;
+        }
+
+        var controllerActionDescriptor = context.GetEndpoint()?.Metadata.GetMetadata<ControllerActionDescriptor>();
+        var controllerType = controllerActionDescriptor?.ControllerTypeInfo.AsType();
+
+        return controllerType is null || !_excludedControllers.Contains(controllerType);
+    }
+}
--- Assemblies/AllYourGoods.Api/Program.cs
+++ Assemblies/AllYourGoods.Api/Program.cs
@@ -1,10 +1,11 @@
-
 using AllYourGoods.Api.Data;
+using AllYourGoods.Api.Extensions;
 using AllYourGoods.Api.Interfaces.Repositories;
 using AllYourGoods.Api.Interfaces.Services;
 using AllYourGoods.Api.Mappings;
 using AllYourGoods.Api.Repositories;
 using AllYourGoods.Api.Services;
+using Azure.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace AllYourGoods.Api;
@@ -14,27 +15,48 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = CreateBuilder(args);
-        var app = builder.Build(); 
-        
+        var app = builder.Build();
+
         InitializeDatabase(app);
-        
+
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (!app.Environment.IsProduction())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
-
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "AllYourGoods API V1");
+                options.RoutePrefix = "";
+            });
         }
 
         app.UseHttpsRedirection();
         app.UseAuthorization();
+
+        app.EnableBuffering();
+        app.EnableRequestBodyLoggingMiddleware();
+
         app.MapControllers();
+        app.MapHealthChecks("/health");
         app.Run();
     }
 
     private static WebApplicationBuilder CreateBuilder(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        if (!builder.Environment.IsDevelopment())
+        {
+            var keyVaultUri = builder.Configuration["KEY_VAULT_URI"];
+            builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri!), new DefaultAzureCredential());
+        }
+
+        builder.Services.AddApplicationInsightsTelemetry(options =>
+        {
+            options.EnableAdaptiveSampling = false;
+        });
+
+        builder.Logging.AddApplicationInsights();
 
         // Add services to the container.
         builder.Services.AddAutoMapper(typeof(RestaurantMappingProfile));
@@ -43,10 +65,11 @@ public class Program
 
         builder.Services.AddControllers();
         builder.Services.AddDbContext<ApplicationContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnectionString")));
 
-        builder.Services.AddHealthChecks();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddHealthChecks()
+            .AddDbContextCheck<ApplicationContext>();
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
