bfa1d6ca9eba179047c65aaf42dad2eda2908b11
soumaiaMamoo <0969581@hr.nl>
2024-10-15 12:11:10 +0200

    feedback fixes

--- Assemblies/AllYourGoods.Api/Models/UserRoles.cs
+++ Assemblies/AllYourGoods.Api/Models/UserRoles.cs
@@ -7,7 +7,7 @@ namespace AllYourGoods.Api.Models
         public Guid UserId { get; set; }
         public virtual User User { get; set; }
 
-        public string RoleId { get; set; }
+        public Guid RoleId { get; set; }
         public virtual Roles Role { get; set; }
     }
 
