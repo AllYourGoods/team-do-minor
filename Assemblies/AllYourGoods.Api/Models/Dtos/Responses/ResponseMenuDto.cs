02ff274003b0d764a12289251e51781bfb811277
Faroeq <alves_1044@hotmail.com>
2024-10-23 15:03:41 +0200

    Update Menu Dto

--- Assemblies/AllYourGoods.Api/Models/Dtos/Responses/ResponseMenuDto.cs
+++ Assemblies/AllYourGoods.Api/Models/Dtos/Responses/ResponseMenuDto.cs
@@ -2,24 +2,32 @@ namespace AllYourGoods.Api.Models.Dtos.Responses;
 
 public class ResponseMenuDto
 {
-    public string id { get; set; }
-    public string name { get; set; }
-    public bool active { get; set; }
-    public List<CategoryDto> categories { get; set; }
+    public ResponseMenuDto(string id, string name, bool active, List<CategoryDto> categories)
+    {
+        Id = id;
+        Name = name;
+        Active = active;
+        Categories = categories;
+    }
+
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public bool Active { get; set; }
+    public List<CategoryDto> Categories { get; set; }
 }
 
 public class CategoryDto
 {
-    public string id { get; set; }
-    public string name { get; set; }
-    public List<ProductDto> products { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public List<ProductDto> Products { get; set; }
 }
 
 public class ProductDto
 {
-    public string id { get; set; }
-    public string name { get; set; }
-    public string description { get; set; }
-    public int price { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
     public string imageUrl { get; set; }
 }
